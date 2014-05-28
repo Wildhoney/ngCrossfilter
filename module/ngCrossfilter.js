7b33a2ebca011b404c9202aca9d85dc92891544f
Wildhoney <adam.timberlake@gmail.com>
2014-05-28 12:06:14 +0100

    Added setup method and getProperties

--- module/ngCrossfilter.js
+++ module/ngCrossfilter.js
@@ -19,10 +19,10 @@
          * @module ngCrossfilter
          * @submodule ngCrossfilterService
          */
-        var Service = function ngCrossfilterService(collection) {
+        var Service = function ngCrossfilterService(collection, primaryKey) {
 
             // Initialise the Crossfilter with the array of models.
-            this._initialise(collection);
+            this._initialise(collection, primaryKey);
 
         };
 
@@ -35,16 +35,51 @@
             /**
              * @property collection
              * @type {crossfilter}
+             * @private
              */
-            collection: {},
+            _collection: {},
+
+            /**
+             * @property _dimensions
+             * @type {Array}
+             * @private
+             */
+            _dimensions: [],
+
+            /**
+             * @property _primaryKey
+             * @type {String|null}
+             * @private
+             */
+            _primaryKey: null,
 
             /**
              * @method _initialise
              * @param collection {Array}
+             * @param primaryKey {String}
              * @return {void}
              * @private
              */
-            _initialise: function _initialise(collection) {},
+            _initialise: function _initialise(collection, primaryKey) {
+
+                // Discover the unique properties in the collection.
+                var properties = this._getProperties(collection[0]),
+                    dimensions = this._dimensions;
+
+                // Initialise the Crossfilter collection, with the primary key.
+                this._collection = $crossfilter(collection);
+                this._primaryKey = primaryKey || properties[0];
+
+                    // Iterate over each property to create its related dimension.
+                $angular.forEach(properties, function(property) {
+
+                    dimensions[property] = _crossfilter.dimension(function(model) {
+                        return model[property];
+                    });
+
+                });
+
+            },
 
             /**
              * @method filterBy
@@ -101,7 +136,29 @@
              * @param number {Number}
              * @return {void}
              */
-            pageNumber: function pageNumber(number) {}
+            pageNumber: function pageNumber(number) {},
+
+            /**
+             * @method _getProperties
+             * @param model {Object}
+             * @return {Array}
+             * @private
+             */
+            _getProperties: function _getProperties(model) {
+
+                var properties = [];
+
+                for (var property in model) {
+
+                    if (model.hasOwnProperty(property)) {
+                        properties.push(property);
+                    }
+
+                }
+
+                return properties;
+
+            }
 
         };
 
